e059b32213581f5183aad0d654cd0623a7a143fc
Ivan Kavanagh <ikavanagh@luc.edu>
2018-09-09 05:02:12 +0000

    added queue in the process of implementing custom array list

--- src/Board.java
+++ src/Board.java
@@ -3,12 +3,19 @@ public class Board
     private String[] board;
     private static String X = "X";
     private static String O = "O";
+    private boolean isConsecutive = false;
+    private static int ROWS = 3; 
+    private static int COLUMNS = 3;
     private static int MAX = 9;
+    private Queue<Computer> Comp = new LinkedList<>();
+    private Queue<Player> p1 = new LinkedList<>();
+    
     
     
     
     public array createBoard()
     {
+        //backend representation of the board
         for(int i = 0;i < MAX;i++)
         {
             board[i] = " ";
@@ -16,14 +23,18 @@ public class Board
         return board;
     }
     
-    public void displayBoard()
+    public String displayBoard()
     {
-        //create character representation here
+        
+        //create visual representation here
+        
     }
     
     public void updateBoard()
     {
         //notify displayBoard() when array is changed
+        //notify score if score is consecutive 
+        
     }
     
     
