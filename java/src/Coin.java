bb35ec1c6a841cc70fb604e210005ab07464eb88
dynamicallocation <ikavanagh@luc.edu>
2018-09-15 03:52:32 +0000

    initial push to new repository

---
+++ java/src/Coin.java
@@ -0,0 +1,15 @@
+public class Coin
+{
+    private ArrayList<String> sides = new ArrayList<String>;
+    private String answer;
+    public Coin(String answer)
+    {
+        this.answer = answer;
+    }
+    
+    public boolean getAnswer()
+    {
+        
+    }
+
+}
---
+++ java/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main 
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        
+        System.out.println("Welcome to Tic Tac Toe Please select a username");
+        
+        
+    }
+
+}
